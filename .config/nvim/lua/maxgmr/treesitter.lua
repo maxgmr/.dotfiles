1b273fb600e95f1db14a7b82759dcfb6855d7f21
maxgmr <mgilmour@maxgmr.ca>
2024-10-24 11:56:23 -0700

    add custom mfs16 filetype

--- .config/nvim/lua/maxgmr/treesitter.lua
+++ .config/nvim/lua/maxgmr/treesitter.lua
@@ -1,15 +1,25 @@
 local configs = require("nvim-treesitter.configs")
-configs.setup {
-  ensure_installed = "all",
-  sync_install = false,
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  autopairs = {
-      enable = true,
-  },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = false,
-  },
-  indent = { enable = false },
-}
+configs.setup({
+	ensure_installed = "all",
+	sync_install = false,
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	autopairs = {
+		enable = true,
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = false,
+	},
+	indent = { enable = false },
+})
+
+-- Create .mfs16 filetype
+vim.filetype.add({
+	extension = {
+		mfs16 = "mfs16",
+	},
+})
+
+-- Add .mfs16 ASM extension
+vim.treesitter.language.register("asm", "mfs16")
