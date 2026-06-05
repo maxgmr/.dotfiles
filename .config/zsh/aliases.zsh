7e3289b2ba76db52cbba0f59ca19d77b05cfde71
Max Gilmour <mgilmour@maxgmr.ca>
2026-06-05 15:52:18 -0700

    zsh: Add alias for git portage

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias ls="lsd"
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias git-portage="/usr/bin/git --git-dir=/etc/portage/portage.git/ --work-tree=/etc/portage"
 alias hl="dbus-run-session start-hyprland"
