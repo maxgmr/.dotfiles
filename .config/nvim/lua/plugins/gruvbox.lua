d37e4d8e6967f68fe8fe7bdd5d9cac2d6952b50e
Max Gilmour <mgilmour@maxgmr.ca>
2026-06-05 15:04:00 -0700

    nvim: Add gruvbox colour scheme

---
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,10 @@
+return {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function(_, opts)
+        require("gruvbox").setup(opts)
+        vim.cmd("colorscheme gruvbox")
+    end,
+}
