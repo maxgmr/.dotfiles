6976b7e84cc8b42fcec08bb960fa6c9038dc9d01
Max Gilmour <mgilmour@maxgmr.ca>
2026-06-09 13:23:04 -0700

    nvim: Add rust-analyzer

---
+++ .config/nvim/lua/lsps/rust_analyzer.lua
@@ -0,0 +1,21 @@
+---@type vim.lsp.Config
+local config = {
+    ---@type lspconfig.settings.rust_analyzer
+    settings = {
+        ["rust-analyzer"] = {
+            diagnostics = {
+                cargo = {
+                    features = "all",
+                },
+                check = {
+                    command = "clippy",
+                },
+                styleLints = {
+                    enable = true,
+                },
+            },
+        }
+    }
+}
+vim.lsp.config("rust_analyzer", config)
+vim.lsp.enable("rust_analyzer")
