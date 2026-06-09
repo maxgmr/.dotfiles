9e0ad008c7951ccd342f013f9195c56c0a385161
Max Gilmour <mgilmour@maxgmr.ca>
2026-06-08 19:35:52 -0700

    nvim: Add more keymaps

--- .config/nvim/lua/config/base_keymaps.lua
+++ .config/nvim/lua/config/base_keymaps.lua
@@ -15,5 +15,19 @@ set_keymap("n", "<leader>cl", ":nohlsearch<CR>", { desc = "Clear search highligh
 set_keymap("v", "<", "<gv", { desc = "Remain in indent mode" })
 set_keymap("v", ">", ">gv", { desc = "Remain in indent mode" })
 
--- Stop yank rewrite on paste
 set_keymap("v", "p", '"_dP', { desc = "Stop yank rewrite on paste" })
+
+set_keymap("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+set_keymap("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+set_keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+set_keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+set_keymap("n", "<leader>pa", function()
+    local path = vim.fn.expand("%:p")
+    vim.fn.setreg("+", path)
+    print("file:", path)
+end, { desc = "Copy full file path" })
+
+set_keymap("n", "<leader>td", function()
+    vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { desc = "Toggle diagnostics" })
