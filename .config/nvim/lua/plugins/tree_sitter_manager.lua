cf2cb688bab0f99d38631c5eeb6be6eed1956ed4
Max Gilmour <mgilmour@maxgmr.ca>
2026-06-09 11:10:41 -0700

    Install all tree sitter parsers

--- .config/nvim/lua/plugins/tree_sitter_manager.lua
+++ .config/nvim/lua/plugins/tree_sitter_manager.lua
@@ -2,8 +2,7 @@ vim.pack.add({
     "https://github.com/romus204/tree-sitter-manager.nvim"
 })
 require("config.utils").safe_require("tree-sitter-manager").setup({
-    ensure_installed = {
-    },
+    ensure_installed = "all",
     auto_install = false,
     highlight = true,
     -- Use regex highlighting for specific languages
