8d036a1ccb92a07ac6e462282e8270888662013e
Max Gilmour <mgilmour@maxgmr.ca>
2026-06-10 19:32:45 -0700

    nvim: Define Python host path for performance

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- Define Python host path (requires pynvim to be installed)
+-- luacheck: ignore 122
+vim.g.python3_host_prog = "/usr/bin/python3"
+
 -- Ensure basic options and keymaps are set up before anything else
 require("config.options")
 require("config.base_keymaps")
