fd9190c549e4cb515f78c78b6d9d4631608928b7
Max Gilmour <mgilmour@maxgmr.ca>
2026-06-16 13:38:20 -0700

    nvim: Wait to configure/activate tools until installation complete

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,22 @@
 local globals = require("config.globals")
 local safe_require = require("config.utils").safe_require
 
+-- Configure and activate tools once tool installation/update is
+-- complete
+vim.api.nvim_create_autocmd("User", {
+    desc = "Configure/activate tools once insallation complete",
+    group = vim.api.nvim_create_augroup("UserMason", { clear = true }),
+    pattern = "MasonToolsUpdateCompleted",
+    callback = function()
+        -- Configure and activate installed tools
+        vim.pack.add({
+            "https://github.com/neovim/nvim-lspconfig",
+            "https://github.com/mason-org/mason-lspconfig.nvim",
+        })
+        safe_require("mason-lspconfig").setup({})
+    end,
+})
+
 vim.schedule(function()
     vim.pack.add({
         "https://github.com/mason-org/mason.nvim",
@@ -55,11 +71,4 @@ vim.schedule(function()
         -- force-updating everything
         mti.check_install(false)
     end
-
-    -- Configure and activate installed tools
-    vim.pack.add({
-        "https://github.com/neovim/nvim-lspconfig",
-        "https://github.com/mason-org/mason-lspconfig.nvim",
-    })
-    safe_require("mason-lspconfig").setup({})
 end)
