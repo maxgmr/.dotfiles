21dedaf343c78ab18c20e62c5bd9314aae6d7963
Max Gilmour <mgilmour@maxgmr.ca>
2026-06-18 10:30:08 -0700

    nvim: Lazy load completion

--- .config/nvim/lua/plugins/blink_cmp.lua
+++ .config/nvim/lua/plugins/blink_cmp.lua
@@ -1,43 +1,52 @@
 local safe_require = require("config.utils").safe_require
 
-vim.pack.add({
-    "https://github.com/saghen/blink.lib",
-    "https://github.com/saghen/blink.cmp",
-    "https://github.com/L3MON4D3/LuaSnip",
-})
-
-local cmp = safe_require("blink.cmp")
-if cmp then
-    cmp.build():pwait()
-    cmp.setup({
-        keymap = {
-            preset = "none",
-            ["<C-Space>"] = { "show", "hide" },
-            ["<Tab>"] = { "accept", "fallback" },
-            ["<C-j>"] = { "select_next", "fallback" },
-            ["<C-k>"] = { "select_prev", "fallback" },
-            ["<C-Right>"] = { "snippet_forward", "fallback" },
-            ["<C-Left>"] = { "snippet_backward", "fallback" },
-        },
-        appearance = { nerd_font_variant = "mono" },
-        completion = {
-            accept = { auto_brackets = { enabled = true } },
-            menu = { auto_show = true },
-        },
-        sources = { default = { "lsp", "path", "buffer", "snippets" } },
-        snippets = {
-            expand = function(snippet)
-                local luasnip = safe_require("luasnip")
-                if luasnip then
-                    luasnip.lsp_expand(snippet)
-                end
-            end,
-        },
-        fuzzy = { implementation = "rust" },
+local function load_plugin()
+    vim.pack.add({
+        "https://github.com/saghen/blink.lib",
+        "https://github.com/saghen/blink.cmp",
+        "https://github.com/L3MON4D3/LuaSnip",
     })
+
+    local cmp = safe_require("blink.cmp")
+    if cmp then
+        cmp.build():pwait()
+        cmp.setup({
+            keymap = {
+                preset = "none",
+                ["<C-Space>"] = { "show", "hide" },
+                ["<Tab>"] = { "accept", "fallback" },
+                ["<C-j>"] = { "select_next", "fallback" },
+                ["<C-k>"] = { "select_prev", "fallback" },
+                ["<C-Right>"] = { "snippet_forward", "fallback" },
+                ["<C-Left>"] = { "snippet_backward", "fallback" },
+            },
+            appearance = { nerd_font_variant = "mono" },
+            completion = {
+                accept = { auto_brackets = { enabled = true } },
+                menu = { auto_show = true },
+            },
+            sources = { default = { "lsp", "path", "buffer", "snippets" } },
+            snippets = {
+                expand = function(snippet)
+                    local luasnip = safe_require("luasnip")
+                    if luasnip then
+                        luasnip.lsp_expand(snippet)
+                    end
+                end,
+            },
+            fuzzy = { implementation = "rust" },
+        })
+    end
+
+    -- luacheck: ignore 122
+    vim.lsp.config["*"] = {
+        capabilities = safe_require("blink.cmp").get_lsp_capabilities(),
+    }
 end
 
--- luacheck: ignore 122
-vim.lsp.config["*"] = {
-    capabilities = safe_require("blink.cmp").get_lsp_capabilities(),
-}
+vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter" }, {
+    desc = "Lazy load completion and snippets",
+    group = vim.api.nvim_create_augroup("CompletionLazyLoad", { clear = true }),
+    once = true,
+    callback = load_plugin,
+})
